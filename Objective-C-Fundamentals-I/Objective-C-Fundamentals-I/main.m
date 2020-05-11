6bfad5b5fe1f5cd70b389a128c4b21f9d3b4ce9b
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-11 17:17:03 +0100

    Added Starter Project

---
+++ Objective-C-Fundamentals-I/Objective-C-Fundamentals-I/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C-Fundamentals-I
+//
+//  Created by Tobi Kuyoro on 11/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
